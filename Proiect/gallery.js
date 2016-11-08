9d8617a16f42e82ec994641a6d66a4098c037887
Blackstar <123blackstar123@gmail.com>
2016-11-09 01:35:42 +0200

    updated gallery to support multi instance

--- Proiect/gallery.js
+++ Proiect/gallery.js
@@ -1,67 +1,93 @@
-var images = [
-  'img/Gallery1/1.jpg',
-  'img/Gallery1/2.jpg',
-  'img/Gallery1/3.jpg',
-  'img/Gallery1/4.jpg',
-  'img/Gallery1/5.jpg',
-  'img/Gallery1/6.jpg',
-  'img/Gallery1/7.jpg',
-  'img/Gallery1/8.jpg'
-];
-var imageIndex = 0;
+function initGallery(rootElement, images) {
+    var imageIndex = 0;
 
-// preload images
-for (var i = 0; i < images.length; i++) {
-  var imageURL = images[i];
-  var image = document.createElement('img');
-  image.src = imageURL;
+    // preload images
+    for (var i = 0; i < images.length; i++) {
+        var imageURL = images[i];
+        var image = document.createElement('img');
+        image.src = imageURL;
 
-  image.onload = function() {
-    console.log('preloaded image');
-  }
-}
+        image.onload = function () {
+            console.log('preloaded image');
+        }
+    }
 
-function setImage(element, index) {
-  element.style.backgroundImage = 'url(' + images[index] + ')';
-}
-function gallerySlider(direction) {
-  if (direction === 'right') {
-    imageIndex++;
-  } else {
-    imageIndex++;
-  }
-  if (imageIndex < 0) {
-    imageIndex = images.length
-  }
-  if (imageIndex >= images.length) {
-    imageIndex = 0;
-  }
+    var imagePad = rootElement.getElementsByClassName('image-pad')[0];
 
-  var div = document.createElement('div');
-  div.className = 'slider-reset ' + direction;
-  setImage(div, imageIndex);
+    function setImage(element, index) {
+        element.style.backgroundImage = 'url(' + images[index] + ')';
+    }
 
-  var imagePad = document.getElementById('image-pad');
-  imagePad.appendChild(div);
+    function gallerySlider(direction) {
+        if (direction === 'right') {
+            imageIndex++;
+        } else {
+            imageIndex++;
+        }
+        if (imageIndex < 0) {
+            imageIndex = images.length
+        }
+        if (imageIndex >= images.length) {
+            imageIndex = 0;
+        }
 
-  // avem timeout pt ca se modifica clasa prea repede nu apuca sa isi dea browser seaman ca vrem 2 lucuri direrite
-  setTimeout(function(){
-    div.className = 'slider-reset center';
-  }, 10);
+        var div = document.createElement('div');
+        div.className = 'slider-reset ' + direction;
+        setImage(div, imageIndex);
 
-  setTimeout(function() {
+        imagePad.appendChild(div);
+
+        // avem timeout pt ca se modifica clasa prea repede nu apuca sa isi dea browser seaman ca vrem 2 lucuri direrite
+        setTimeout(function () {
+            div.className = 'slider-reset center';
+        }, 10);
+
+        setTimeout(function () {
+            setImage(imagePad, imageIndex);
+            imagePad.removeChild(div);
+        }, 210);
+    }
+
     setImage(imagePad, imageIndex);
-    imagePad.removeChild(div);
-  }, 210);
+
+
+    rootElement.getElementsByClassName('left-pad')[0].onclick = function () {
+        gallerySlider('left');
+    };
+
+    rootElement.getElementsByClassName('right-pad')[0].onclick = function () {
+        gallerySlider('right');
+    };
 }
 
-setImage(document.getElementById('image-pad'), imageIndex);
+window.onload = function () {
+    initGallery(
+        document.getElementById('gallery1'),
+        [
+            'img/Gallery1/1.jpg',
+            'img/Gallery1/2.jpg',
+            'img/Gallery1/3.jpg',
+            'img/Gallery1/4.jpg',
+            'img/Gallery1/5.jpg',
+            'img/Gallery1/6.jpg',
+            'img/Gallery1/7.jpg',
+            'img/Gallery1/8.jpg'
+        ]
+    );
+
+    initGallery(
+        document.getElementById('gallery2'),
+        [
+            'img/arch1/1.jpg',
+            'img/arch1/2.jpg',
+            'img/arch1/3.jpg',
+            'img/arch1/4.jpg',
+            'img/arch1/5.jpg',
+            'img/arch1/6.jpg',
+            'img/arch1/7.jpg',
+            'img/arch1/8.jpg'
+        ]
+    );
 
 
-document.getElementById('left-pad').onclick = function() {
-  gallerySlider('left');
-};
-
-document.getElementById('right-pad').onclick = function() {
-  gallerySlider('right');
 };
